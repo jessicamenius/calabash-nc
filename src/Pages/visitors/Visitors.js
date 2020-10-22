222db229b43c19a3de58846693c66ce27bb7757a
jessicamenius <37314892+jessicamenius@users.noreply.github.com>
2020-10-22 17:28:49 -0500

    adding materialize

---
+++ src/Pages/visitors/Visitors.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Visitors() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Visitors
--- src/components/Side_Navbar.js
+++ src/components/Side_Navbar.js
@@ -1,30 +1,11 @@
 import React from 'react'
 
 function Side_Navbar() {
-    return (
-        <div>
-            
-            {/* <nav> <!-- navbar content here  --> </nav>
-
-<ul id="slide-out" className="sidenav sidenav-fixed">
-  <li><div className="user-view">
-    <div className="background">
-      <img src="images/office.jpg">
+  return (
+    <div>
+      
     </div>
-    <a href="#user"><img className="circle" src="images/yuna.jpg"></a>
-    <a href="#name"><span className="white-text name">John Doe</span></a>
-    <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
-  </div></li>
-  <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
-  <li><a href="#!">Second Link</a></li>
-  <li><div className="divider"></div></li>
-  <li><a className="subheader">Subheader</a></li>
-  <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
-</ul>
-<a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a> */}
-
-        </div>
-    )
+  )
 }
 
 export default Side_Navbar
